bea0e37498b5a203813ddbba9c731ca1cdaf0ef6
denisecodes <denise.chan112@gmail.com>
2023-09-19 15:28:15 +0100

    add deploy stage to build-ci.yml and add variable name for INSTANCE_IP_ADDRESS

--- scripts/deploy-to-instance.sh
+++ scripts/deploy-to-instance.sh
@@ -6,8 +6,8 @@ echo "$SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add -
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
 
-# The instance ip address is 52.12.34.133
+# The instance ip address is 18.134.252.24
 # we should probably avoid hardcoding this, in case
 # the IP changes
-rsync -av -e "ssh -o StrictHostKeyChecking=no" ./* ec2-user@52.12.34.133:/var/acebook/
-ssh -o StrictHostKeyChecking=no ec2-user@52.12.34.133 "sudo systemctl restart acebook"
+rsync -av -e "ssh -o StrictHostKeyChecking=no" ./* ec2-user@$INSTANCE_IP_ADDRESS:/var/acebook/
+ssh -o StrictHostKeyChecking=no ec2-user@$INSTANCE_IP_ADDRESS "sudo systemctl restart acebook"
